35c07566fd12b896c0d73c06d215db044e074108
slaccware <54006754+slackwaree@users.noreply.github.com>
2024-09-17 16:45:26 +1200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-
+const slack = document.getElementById("slackimg");
+var audio = new Audio("https://nonprod-audio.dictionary.com/P05/P0566600/P0566600.mp3");
+slack.addEventListener("click", function {
+  audio.play();
+})
