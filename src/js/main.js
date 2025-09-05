750e1d80b955781e661bdfab646f4d6bba31a534
slackwaree <54006754+slackwaree@users.noreply.github.com>
2025-09-05 14:31:33 +1200

    Move js from dist/ into src/ (Moved away from TS and back to JS). Fix bug where getCurrentDate.getMinutes() returned single digit if getMinutes() was less than 10.

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+var currentTime = document.getElementById('current-time');
+var currentDay = document.getElementById('current-day');
+var TZ_OFFSET = -720;
+var getCurrentDate;
+
+var DAYS_LIST = ['Monday', 'Tuesday', 'Wednesday',
+    'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+function setDate() {
+    var date = new Date();
+    getCurrentDate = new Date(date.getTime() + (TZ_OFFSET + date.getTimezoneOffset()) * 60 * 1000);
+    minToString = getCurrentDate.getMinutes().toString();
+    if (minToString.length == 1) {
+        currentTime.textContent = "".concat(getCurrentDate.getHours(), ":0").concat(getCurrentDate.getMinutes());
+    }
+    else {
+        currentTime.textContent = "".concat(getCurrentDate.getHours(), ":").concat(getCurrentDate.getMinutes());
+    }
+    currentDay.textContent = DAYS_LIST[getCurrentDate.getDay()];
+    setDateAsync();
+}
+setDate();
+function setDateAsync() {
+    setTimeout(function () {
+        setDate();
+    }, 1000);
+}
