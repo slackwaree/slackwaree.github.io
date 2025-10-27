bb5bf4498bb02a3b7e12e265f80326091389f68c
slackwaree <54006754+slackwaree@users.noreply.github.com>
2025-10-27 15:55:08 +1300

    did thinkg idfk

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,44 @@
-var currentTime = document.getElementById('current-time');
-var currentDay = document.getElementById('current-day');
-var TZ_OFFSET = -720;
-var getCurrentDate;
+let currentTime = document.getElementById('current-time');
+let currentDay = document.getElementById('current-day');
+const TZ_OFFSET = -720;
+let dstOffset;
+let isDaylightSavings;
+let getCurrentDate;
 
-var DAYS_LIST = ['Monday', 'Tuesday', 'Wednesday',
+const DAYS_LIST = ['Monday', 'Tuesday', 'Wednesday',
     'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
+setDate();
+function checkDST(getCurrentDate) {
+    if ( // date is between septemper and april?
+        getCurrentDate.getMonth() <= 8 ||
+        getCurrentDate.getMonth() <= 3
+        ) {
+        if ( // date < 26th && month = september && day != sunday?
+            getCurrentDate.getDate() <= 25 &&
+            getCurrentDate.getMonth() == 8 &&
+            getCurrentDate.getDay() != 0
+        ) {
+            dstOffset = 0;
+        }
+        else if (
+            getCurrentDate.getDate() = 7 &&
+            getCurrentDate.getMonth() == 3 &&
+            getCurrentDate.getDay() != 0
+        ) {
+            dstOffset = 0;
+        }
+    }
+    else {
+        dstOffset = 3600 / 60; // DST difference
+        console.log('h');
+    }
+}
 function setDate() {
-    var date = new Date(); // This function was written in TS if it looks off
-    getCurrentDate = new Date(date.getTime() + (TZ_OFFSET + date.getTimezoneOffset()) * 60 * 1000);
-    minToString = getCurrentDate.getMinutes().toString();
+    let date = new Date(); 
+    let getCurrentDate = new Date(date.getTime() + (TZ_OFFSET + dstOffset + date.getTimezoneOffset()) * 60 * 1000);
+    let minToString = getCurrentDate.getMinutes().toString();
+    checkDST(getCurrentDate);
     if (minToString.length == 1) {
         currentTime.textContent = "".concat(getCurrentDate.getHours(), ":0").concat(getCurrentDate.getMinutes());
     }
@@ -19,7 +48,6 @@ function setDate() {
     currentDay.textContent = DAYS_LIST[getCurrentDate.getDay()];
     setDateAsync();
 }
-setDate();
 function setDateAsync() {
     setTimeout(function () {
         setDate();
