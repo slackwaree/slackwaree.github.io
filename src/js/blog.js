42eb85a64701ded038f46d799af3b7967d87143b
slackwaree <54006754+slackwaree@users.noreply.github.com>
2025-11-12 15:45:57 +1300

    Temporarily comment-out fetch request

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,13 +1,18 @@
 
-async function loadPost(postNum) {
-    postNum = 0;
-    const url = `https://d20ryw3o58mey6.cloudfront.net/posts/${postNum}.md`;
-    const res = await fetch(url, {
-        method: 'GET',
-        mode: 'cors',
-    });
-    if (!res.ok) throw new Error();
-    const md = await res.text();
-    console.log(md);
-}
-window.addEventListener('load', loadPost());
+// async function loadPost(postNum) {
+//     postNum = 0;
+//     const url = `https://d20ryw3o58mey6.cloudfront.net/posts/${postNum}.md`;
+//     const res = await fetch(url, {
+//         method: 'GET',
+//         mode: 'cors',
+//     });
+//     if (!res.ok) throw new Error();
+//     const md = await res.text();
+//     console.log(md);
+// }
+// window.addEventListener('load', loadPost());
+import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js';
+// or const { marked } = require('marked');
+
+const renderToHTML = marked.parse('# Marked in Node.js\n\nRendered by **marked**.');
+console.log(renderToHTML)
