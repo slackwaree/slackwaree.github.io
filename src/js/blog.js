1bbbbe3b07a0bc8d08ce261c8bd11e17c445b51b
slackwaree <54006754+slackwaree@users.noreply.github.com>
2025-11-14 14:30:54 +1300

    Finalize

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,21 +1,35 @@
 import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js';
 
-async function loadPost(postNum) {
-    postNum = 0;
+let postNum = 0;
+window.addEventListener('load', () => {
+    loadPost(postNum++);
+});
+
+async function loadPost(postNum) { // posts are named in ascending numerical order
     const url = `https://d20ryw3o58mey6.cloudfront.net/posts/${postNum}.md`;
     const res = await fetch(url, {
         method: 'GET',
         mode: 'cors',
     });
-    if (!res.ok) throw new Error();
-    const md = await res.text();
-    console.log(md);
-
-    renderToHTML(md);
+    if (!res.ok) throw new Error('No more posts avaliable!');
+    const getMD = await res.text();
+    renderToHTML(getMD);
 }
 
-function renderToHTML(md) {
-    const renderToHTML = marked.parse(md);
-    console.log(renderToHTML);
+function renderToHTML(getMD) {
+    const blogContainer = document.querySelector('.blog-container');
+    const createArticle = document.createElement('article');
+    const renderToHTML = marked.parse(getMD);
+
+    createArticle.innerHTML = renderToHTML; // Security headers are active to prevent XSS
+    blogContainer.appendChild(createArticle);
+
+    if (postNum = 1) {
+        removeHoldState()
+    }
 }
-window.addEventListener('load', loadPost());
+
+function removeHoldState() {
+    const holdStatusText = document.querySelector('#hold-status');
+    holdStatusText.remove();
+}
